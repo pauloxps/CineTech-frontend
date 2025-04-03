5289986a0bbdce8e9ae2ed455d272f722b01edce
pauloxps <pauloxpscardoso@gmail.com>
2025-04-03 17:15:50 -0300

    Atualizações no frontend

--- js/cadastrarFilme.js
+++ js/cadastrarFilme.js
@@ -1,16 +1,14 @@
 document.getElementById("formFilme").addEventListener("submit", async (e) => {
-    e.preventDefault();
-  
-    const dados = Object.fromEntries(new FormData(e.target).entries());
-  
-    const resposta = await fetch("http://localhost:8000/api/filmes.php", {
+  e.preventDefault();
+
+  const formData = new FormData(e.target); // Inclui o arquivo no envio
+
+  const resposta = await fetch("http://localhost:8000/api/filmes.php", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(dados)
-    });
-  
-    const resultado = await resposta.json();
-    alert(resultado.mensagem || resultado.erro || "Erro desconhecido");
-    e.target.reset();
+      body: formData // Envia o formulário como multipart/form-data
   });
-  
+
+  const resultado = await resposta.json();
+  alert(resultado.mensagem || resultado.erro || "Erro desconhecido");
+  e.target.reset();
+});
